6d1ea44e4cd8ef87c21b280d80a16d0e306fbdac
maxamadBashi <165473340+maxamadBashi@users.noreply.github.com>
2025-12-23 10:40:34 +0300

    Change axios base URL to production endpoint

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://ayatiin-backend.onrender.com',
 });
 
 // Add a request interceptor to add the token to requests
